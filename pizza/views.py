19d743cab83c21d4f8a01e6bdafe9576bdfe92a6
alex1dev <alexander.w.jv@gmail.com>
2018-04-08 16:00:28 +0000

    pizza

--- pizza/views.py
+++ pizza/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from pizza.forms import ContactForm
 
+from django.core.mail import send_mail, BadHeaderError
+# Create your views here.
 def index(request):
-    return render(request, 'pizza/index.html')
+	form = ContactForm(request.POST)
+
+	if request.method == 'POST':
+		
+		
+		if form.is_valid():
+			subject = form.cleaned_data['subject']
+			email = form.cleaned_data['email']
+			message = form.cleaned_data['message']
+			try:
+				send_mail(subject,message, email, ['admin@admin.py'])
+			except BadHeaderError:
+				return HttpResponse('Invalid header found.')
+			return redirect('pizza/index')
+
+	return render(request, "pizza/index.html", {'form' : form})
